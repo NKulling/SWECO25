8a42d961562de53f596408376da40c2a29eaf76a
NKulling <78426848+NKulling@users.noreply.github.com>
2023-08-22 16:11:49 +0200

    Update resampling_impact_analysis.R

--- database_validation/script/resampling_impact_analysis.R
+++ database_validation/script/resampling_impact_analysis.R
@@ -31,12 +31,7 @@ for (file in files) {
   rmsd <- sqrt(mean((df$original - df$sweco)^2, na.rm=T))
 
   normalized_rmsd <- rmsd / (max(df$original) - min(df$original))
-  
-  # Calculate the R-squared 
-  dt <- na.omit(df)
-  correlation <- cor(dt$original, dt$sweco)
-  rsquared <- correlation^2 
-  
+    
   # Extract layer name
   nm <- basename(file)
   
@@ -46,7 +41,6 @@ for (file in files) {
     mean = mn,
     median = md,
     standardDeviation = std,
-    RSquared = rsquared,
     RMSD = rmsd,
     NRMSD = normalized_rmsd
   )
@@ -63,11 +57,6 @@ sd(result_table$NRMSD)        #0.0092
 range(result_table$NRMSD)     #min 0.003558642  max 0.041301552
 nrow(result_table)            # 28 Layers
 
-mean(result_table$RSquared)   #0.9651068
-median(result_table$RSquared) #0.9896692
-sd(result_table$RSquared)     #0.04316473
-range(result_table$RSquared)  # min 0.8551718 max 0.9994933
-
 #- visualisation
 
 #- NRMSD
@@ -79,14 +68,4 @@ ggplot(result_table, aes(x = NRMSD, y = reorder(layername, NRMSD))) +
 
 setwd(figures)
 ggsave("scatterplot_NRMSD.jpg", plot=last_plot(), height= 5, width= 6)
-
-#- R-squared
-ggplot(result_table, aes(x = RSquared, y = reorder(layername, RSquared))) +
-  geom_point(shape = 16, size = 3, position = "identity") +
-  labs(x = "R-squared", y = "") +
-  theme_bw() +
-  scale_x_continuous(limits = c(0, 1))
-
-setwd(figures)
-ggsave("scatterplot_RSq.jpg", plot=last_plot(), height= 5, width= 6)
 
