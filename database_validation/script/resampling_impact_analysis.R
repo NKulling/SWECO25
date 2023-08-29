02382cd4ea922b3a0d48515965abf250068816b7
NKulling <78426848+NKulling@users.noreply.github.com>
2023-08-29 10:18:58 +0200

    Update resampling_impact_analysis.R

--- database_validation/script/resampling_impact_analysis.R
+++ database_validation/script/resampling_impact_analysis.R
@@ -4,6 +4,9 @@ library(ggplot2)
 
 datafold<-"./data"
 
+#####
+# 1) Continuous variables
+#####
 # Files to compare
 files<-list.files(datafold, full.names = T, pattern = "*.rds")
 
@@ -69,3 +72,58 @@ ggplot(result_table, aes(x = NRMSD, y = reorder(layername, NRMSD))) +
 setwd(figures)
 ggsave("scatterplot_NRMSD.jpg", plot=last_plot(), height= 5, width= 6)
 
+
+#####
+# 2) Discrete variables
+#####
+
+datafold<-"./data/discrete"
+
+# Files to compare
+files<-list.files(datafold, full.names = T, pattern = "*.rds")
+
+# Create an empty data frame to store the results
+
+result_table <- data.frame()
+
+# Loop through each file in "files"
+
+for (file in files) {
+  # Load data
+  df <- readRDS(file)
+
+  # Calculate dice coef
+  
+  dice_coefficient <- 2 * sum(df$original == df$sweco) / (length(df$original) + length(df$sweco))
+  
+  # Extract layer name
+  nm <- basename(file)
+  
+  # Create a row with the calculated values
+  row <- data.frame(
+    layername = nm,
+    dice_coef = dice_coefficient
+  )
+  
+  # Append the row to the result table
+  result_table <- rbind(result_table, row)
+}
+
+
+##Descriptive stats: 
+
+mean(result_table$dice_coef)      #0.9412
+median(result_table$dice_coef)    #0.9905
+sd(result_table$dice_coef)        #0.073
+range(result_table$dice_coef)     #min 0.8234  max 1
+nrow(result_table)                #7
+
+
+ggplot(result_table, aes(x = dice_coef, y = reorder(layername, dice_coef))) +
+  geom_point(shape = 16, size = 3, position = "identity") +
+  labs(x = "Dice coefficient", y = "") +
+  theme_bw() +
+  scale_x_continuous(limits = c(0, 1))
+
+setwd(figures)
+ggsave("scatterplot_Dice.jpg", plot=last_plot(), height= 5, width= 6)
